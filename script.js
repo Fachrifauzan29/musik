3d94ffca23b4b752a7704fb5fcaa618a8acdfcf9
Fachrifauzan29 <92680295+Fachrifauzan29@users.noreply.github.com>
2023-01-04 22:11:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ document.addEventListener('DOMContentLoaded', function () {
     },
     {
       audioSrc: './tracks/Yoh Kamiyama  Irokousui.mp3',
-      coverSrc: './images/pic1.jpg',
+      coverSrc: './images/pic2.jpg',
       name: 'Yoh Kamiyama',
       desc: 'Irokousui',
       id: 11,
